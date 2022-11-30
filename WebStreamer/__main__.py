61151c67ddaba9771531fc279a771d3b795b9874
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-11-30 09:50:50 +0530

    Update __main__.py

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -43,13 +43,13 @@ async def start_services():
     print('------------------- Initalizing Web Server -------------------')
     app = web.AppRunner(await web_server())
     await app.setup()
-    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.FQDN
+    bind_address = "0.0.0.0" if Var.ON_RAILWAY else Var.FQDN
     await web.TCPSite(app, bind_address, Var.PORT).start()
     print('\n')
     print('----------------------- Service Started -----------------------')
     print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
     print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
-    if Var.ON_HEROKU:
+    if Var.ON_RAILWAY:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------')
     await idle()
